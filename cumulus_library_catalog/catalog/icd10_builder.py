2bdf8b5cb033a89610bd6621dec5c907f7fafb1f
Matt Garber <matthew.garber@childrens.harvard.edu>
2025-01-21 14:52:57 -0500

    PR Feedback

--- cumulus_library_catalog/catalog/icd10_builder.py
+++ cumulus_library_catalog/catalog/icd10_builder.py
@@ -1,7 +1,6 @@
 import pathlib
 
 import cumulus_library
-from cumulus_library.template_sql import base_templates
 
 
 class ICDCohortBuilder(cumulus_library.BaseTableBuilder):
@@ -9,7 +8,7 @@ class ICDCohortBuilder(cumulus_library.BaseTableBuilder):
     def prepare_queries(self, *args, **kwargs):
         for name in ['cohort','category','block','chapter','code']:
             self.queries.append(
-                base_templates.get_template(
+                cumulus_library.get_template(
                     "icd10",
                     pathlib.Path(__file__).resolve().parent / "templates/",
                     name=name,
