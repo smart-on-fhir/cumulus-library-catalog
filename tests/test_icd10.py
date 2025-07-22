b6fc292831f36b535c37901f451b1afd48bebd8d
Matt Garber <matthew.garber@childrens.harvard.edu>
2025-07-22 10:26:25 -0400

    Split out hierarchy counts into individual tables

--- tests/test_icd10.py
+++ tests/test_icd10.py
@@ -1,36 +1,70 @@
 import pathlib
-from cumulus_library import cli, StudyManifest
+
+import pytest
+from cumulus_library import StudyManifest, cli
 
 from cumulus_library_catalog.catalog import icd10_annotated_counts
 
-
-def test_count_diagnoses(mock_db_core_config):
+@pytest.mark.parametrize(
+    "tier,expected",
+    [
+        ("all",(2,)),
+        ("chapter",(1, 'Certain infectious and parasitic diseases (A00-B99)', 'A00-B99')),
+        ("block",
+            (
+                1,
+                'A00-B99',
+                'Certain infectious and parasitic diseases (A00-B99)',
+                'Viral infections characterized by skin and mucous membrane lesions (B00-B09)',
+                'B00-B09'
+            )
+        ),
+        ("category",
+            (
+                1,
+                'B00-B09',
+                'Viral infections characterized by skin and mucous membrane lesions (B00-B09)',
+                'Measles',
+                'B05'
+            )
+        ),
+        ("subcategory_1",
+            (
+                1,
+                'B05',
+                'Measles',
+                'Measles with other complications',
+                'B05.8'
+            )
+        ),
+        ("subcategory_2",
+            (
+                1,
+                'B05.8',
+                'Measles with other complications',
+                'Other measles complications',
+                'B05.89'
+            )
+        ),
+        ("subcategory_3",
+            ()
+        ),
+        ("extension",
+            ()
+        ),
+    ]
+)
+def test_count_tiers(mock_db_core_config, tier, expected):
     cursor = mock_db_core_config.db.cursor()
  
     manifest=StudyManifest(study_path="./cumulus_library_catalog/")
     builder = icd10_annotated_counts.IcdCountsBuilder(manifest)
     builder.execute_queries(mock_db_core_config,manifest, min_subject=1)
-    res = cursor.execute("select * from catalog__count_icd10_diagnoses").fetchall()
-
-    # There's exactly one ICD10 code in our mock condition data, so we get a small result back.
-    assert res[0] == (
-        1,
-        'B05.89',
-        'A00-B99',
-        'Certain infectious and parasitic diseases (A00-B99)',
-        'B00-B09',
-        'Viral infections characterized by skin and mucous membrane lesions (B00-B09)',
-        'B05',
-        'Measles',
-        'B05.8',
-        'Measles with other complications',
-        'B05.89',
-        'Other measles complications',
-        None,
-        None,
-        None,
-        None
-    )
+    res = cursor.execute(f"select * from catalog__count_icd10_{tier}").fetchall()
+    if tier in ("subcategory_3","extension"):
+        assert res ==[]
+    else:
+        assert res[0] == expected
 
 def test_build_study(mock_db_core_config):
     cursor = mock_db_core_config.db.cursor()
@@ -45,5 +79,5 @@ def test_build_study(mock_db_core_config):
         options={},
     )
     new_count = len(cursor.execute("select * from information_schema.tables").fetchall())
-    assert new_count == table_count + 4
+    assert new_count == table_count + 11
 
